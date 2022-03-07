940df82d346c7e1a256ec70fa3f2aca0344c5c28
David Wesley <david.wesley755@outlook.com>
2022-03-07 20:12:23 -0300

    style: Changed the increment line in loops

--- basecamp/Listas/C/C11/ex02/ft_any.c
+++ basecamp/Listas/C/C11/ex02/ft_any.c
@@ -6,7 +6,7 @@
 /*   By: dwesley <dwesley@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 03:30:24 by dwesley           #+#    #+#             */
-/*   Updated: 2022/03/05 04:26:30 by dwesley          ###   ########.fr       */
+/*   Updated: 2022/03/07 20:09:11 by dwesley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 
 int	ft_any(char **tab, int (*f)(char*))
 {
-	int	index;
+	unsigned int	index;
 
 	index = 0;
 	while (tab[index])
-	{
-		if (f(tab[index]) != 0)
+		if (f(tab[index++]) != 0)
 			return (TRUE);
-		index++;
-	}
 	return (FALSE);
 }
--- basecamp/Listas/C/C11/ex03/ft_count_if.c
+++ basecamp/Listas/C/C11/ex03/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: dwesley <dwesley@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 04:17:33 by dwesley           #+#    #+#             */
-/*   Updated: 2022/03/05 04:25:44 by dwesley          ###   ########.fr       */
+/*   Updated: 2022/03/07 20:09:30 by dwesley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ int	ft_count_if(char **tab, int length, int (*f)(char*))
 
 	index = 0;
 	while (index < length)
-	{
-		if (f(tab[index]) != 0)
+		if (f(tab[index++]) != 0)
 			count++;
-		index++;
-	}
 	return (count);
 }
